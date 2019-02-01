67ecbf9db74989a1de11795b14560a3e9a687d54
Ryu Shinmura <rysestock@gmail.com>
2019-02-01 22:45:37 +0900

    メッセージ関連のインタフェースを作成

---
+++ YouTubeLiveIF/Message.cs
@@ -0,0 +1,43 @@
+﻿using SitePlugin;
+
+namespace YouTubeLiveSitePlugin
+{
+    public enum YouTubeLiveMessageType
+    {
+        Unknown,
+        Comment,
+        Superchat,
+        Connected,
+        Disconnected,
+    }
+
+
+    public interface IYouTubeLiveMessage : IMessage
+    {
+        YouTubeLiveMessageType YouTubeLiveMessageType { get; }
+    }
+    public interface IYouTubeLiveConnected : IYouTubeLiveMessage
+    {
+    }
+    public interface IYouTubeLiveDisconnected : IYouTubeLiveMessage
+    {
+    }
+    public interface IYouTubeLiveComment : IYouTubeLiveMessage
+    {
+        //string Comment { get; }
+        string Id { get; }
+        //string UserName { get; }
+        string UserId { get; }
+        long PostedAt { get; }
+        IMessageImage UserIcon { get; set; }
+    }
+    public interface IYouTubeLiveSuperchat : IYouTubeLiveMessage
+    {
+        //string Comment { get; }
+        string Id { get; }
+        //string UserName { get; }
+        string UserId { get; }
+        long PostedAt { get; }
+        IMessageImage UserIcon { get; }
+    }
+}
